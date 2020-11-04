861b8d11ea72cd476ec9ee31dea0c45fbfcae832
tejing <tejing163@msn.com>
2020-11-04 14:23:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,32 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+cd ../..
+
+cd package/lean/
+[ ! -d luci-app-adguardhome ] && git clone https://github.com/rufengsuixing/luci-app-adguardhome
+cd ../../
+# helloworld
+sed -i -E 's/#(src-git.+)(helloworld.+)/\1\2/' feeds.conf.default
+
+echo -e '\nsrc-git lienol https://github.com/xiaorouji/openwrt-package.git;master' >> feeds.conf.default
+./scripts/feeds update -a
+#rm -rf feeds/lienol/lienol/ipt2socks
+rm -rf feeds/lienol/lienol/shadowsocksr-libev
+rm -rf feeds/lienol/lienol/pdnsd-alt
+rm -rf feeds/lienol/lienol/luci-app-verysync
+rm -rf feeds/lienol/package/verysync
+rm -rf package/lean/ipt2socks
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
+./scripts/feeds install -a
